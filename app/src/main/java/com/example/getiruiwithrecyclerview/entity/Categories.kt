b0b507672bc738e7278648c9e7e331587b6d3506
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-01-25 16:23:49 +0300

    categories data class and categories card design add project

---
+++ app/src/main/java/com/example/getiruiwithrecyclerview/entity/Categories.kt
@@ -0,0 +1,10 @@
+package com.example.getiruiwithrecyclerview.entity
+
+import java.io.Serializable
+
+data class Categories(
+    var categoriName: String,
+    var categoriImage: String,
+) : Serializable {
+
+}

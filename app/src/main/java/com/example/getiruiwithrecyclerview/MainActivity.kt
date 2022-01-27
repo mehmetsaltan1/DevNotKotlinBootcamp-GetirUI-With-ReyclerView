0fb2f6309aa850ceb67306cd5fbc4f11a6445c61
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-01-27 13:27:49 +0300

    the codes were reorganized and the project was terminated

--- app/src/main/java/com/example/getiruiwithrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/getiruiwithrecyclerview/MainActivity.kt
@@ -10,29 +10,54 @@ import com.example.getiruiwithrecyclerview.databinding.ActivityMainBinding
 import com.example.getiruiwithrecyclerview.entity.Categories
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tasarim:ActivityMainBinding
+    private lateinit var tasarim: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tasarim = ActivityMainBinding.inflate(layoutInflater)
-        tasarim.rv.layoutManager = StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL)
+        tasarim.rv.layoutManager =
+            StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL)
         val categoriesList = ArrayList<Categories>()
-        categoriesList.add(Categories("Yeni Ürünler","yeniurunler"))
-        categoriesList.add(Categories("İndirimler","indirimler"))
-        categoriesList.add(Categories("Su & İçecek","icecek"))
-        categoriesList.add(Categories("Meyve & Sebze","meyvesebze"))
-        categoriesList.add(Categories("Fırından","firindan"))
-        categoriesList.add(Categories("Temel Gıda","temelgida"))
-        categoriesList.add(Categories("Atıştırmalık","atistirmalik"))
-        categoriesList.add(Categories("Dondurma","dondurma"))
-        categoriesList.add(Categories("Süt Ürünleri","suturunleri"))
-        categoriesList.add(Categories("Kahvaltılık","kahvaltilik"))
-        categoriesList.add(Categories("Yiyecek","yiyecek"))
-        categoriesList.add(Categories("Fit & Form","fitform"))
-        categoriesList.add(Categories("Kişisel Bakım","kisiselbakim"))
-        categoriesList.add(Categories("Ev Bakım","evbakim"))
-        categoriesList.add(Categories("Ev & Yaşam","evyasam"))
-        categoriesList.add(Categories("Teknoloji","teknoloji"))
-        val adapter = CategoriesAdapter(this,categoriesList)
+        val categoriNameList = arrayOf(
+            "Yeni Ürünler",
+            "İndirimler",
+            "Su & İçecek",
+            "Meyve & Sebze",
+            "Fırından",
+            "Temel Gıda",
+            "Atıştırmalık",
+            "Dondurma",
+            "Süt Ürünleri",
+            "Kahvaltılık",
+            "Yiyecek",
+            "Fit & Form",
+            "Kişisel Bakım",
+            "Ev Bakım",
+            "Ev & Yaşam",
+            "Teknoloji"
+        )
+        val categoriImageList = arrayOf(
+            "yeniurunler",
+            "indirimler",
+            "icecek",
+            "meyvesebze",
+            "firindan",
+            "temelgida",
+            "atistirmalik",
+            "dondurma",
+            "suturunleri",
+            "kahvaltilik",
+            "yiyecek",
+            "fitform",
+            "kisiselbakim",
+            "evbakim",
+            "evyasam",
+            "teknoloji"
+        )
+        for (i in categoriNameList.indices) {
+            categoriesList.add(Categories(categoriNameList[i], categoriImageList[i]))
+        }
+
+        val adapter = CategoriesAdapter(this, categoriesList)
         tasarim.rv.adapter = adapter
         setContentView(tasarim.root)
 
